eb2349c90aeea0d5a27baf28f8f36c0d30786fbe
DevaMotanna <Devamotanna1993@gmail.com>
2025-12-24 10:45:25 +0530

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import MovieDetails from "./pages/MovieDetails";
-import Favorites from './Pages/Favorites'
+import Favorites from './pages/Favorites'
 
 function App() {
   return (
